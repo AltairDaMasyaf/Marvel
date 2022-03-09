0680539f72b1c92cf79c1a8fd403302f947b0364
AltairDaMasyaf <95611920+AltairDaMasyaf@users.noreply.github.com>
2022-03-09 22:07:10 +0300

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+// Collapsible //
+
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
+
+// Change Nav Color //
+
+$(document).ready(function () {
+  var scroll_start = 0;
+  var startchange = $("#hero");
+  var offset = startchange.offset();
+  if (startchange.length) {
+    $(document).scroll(function () {
+      scroll_start = $(this).scrollTop();
+      if (scroll_start > offset.top) {
+        $(".nav").css("background-color", "#000");
+        $(".nav").css("border-bottom", "3px solid red");
+      } else {
+        $(".nav").css("background-color", "transparent");
+        $(".nav").css("border-bottom", "3px solid white");
+      }
+    });
+  }
+});
+
+// NFT
